92f31280f17a4af1fa1517c8a3587b10be192956
Anthony <anthony.gursky@gmail.com>
2014-10-28 14:21:55 +0200

    Update crunchlmde.sh

--- crunchlmde.sh
+++ crunchlmde.sh
@@ -1,33 +1,7 @@
-# ANG: crunchbang Waldorf on LMDE
-# ANG: for debian 8 jessie themes are still broken. Needs to be redone (see below)
-# modified for install after GNOME
-mkdir crunchjessie && pushd crunchjessie
-
-# libvala dependency was renamed to vala in trusty, get an older package
-wget -c https://launchpad.net/~vala-team/+archive/ubuntu/ppa/+files/libvala-0.10-0_0.10.4-1ubuntu1~lucid1_amd64.deb
-sudo dpkg -i libvala-*
-
-# add waldorf repository and install crunchbang packages
-sudo apt-get -y --install-recommends install software-properties-common
-### 
-### ANG: error when adding to lmde: adding manually to 
-### /etc/apt/sources.list.d/official-package-repositories.list
-###
-### sudo apt-add-repository 'deb http://packages.crunchbang.org/waldorf waldorf main'
-sudo apt-key adv --fetch-keys http://packages.crunchbang.org/waldorf-dev/crunchbang.key
-sudo apt-get -y update
-# ANG: use gdm3 ! - try slim later
-sudo apt-get -y --install-recommends install cb-metapackage cairo-compmgr faenza-crunchbang-icon-theme
-# ANG: error during install tint2 - sudo apt-get install -f fixes
-sudo apt-get install -f
-
-# set up home directory
-cp -R /etc/skel/. ~/
-
-# set up boot theme: ANG - needs to be set up later - boot-customizer ?
-sudo cp /etc/default/grub /etc/default/grub.orig
-sudo sed -i -E 's/(GRUB_CMDLINE_LINUX_DEFAULT=).*/\1"--verbose nosplash debug"/g' /etc/default/grub
-sudo update-grub
+# ANG: crunchbang Waldorf on LMDE:
+# ANG: installed as debian 7 netinstall. Next cb-netinstall => crunchbang
+# Next /etc/apt/ changed for /etc/apt/ from LMDE, update/upgrade/dist-upgrade
+# Correct waldorf theme (without fixing vala):
 
 # gtk-3.8+ breaks Waldorf theme, use an alternative
 wget -c https://github.com/xoraxiom/openbox-gtk-themes/archive/master.zip
